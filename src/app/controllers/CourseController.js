77eddde8675340c578c3de62e94d7e711cc7dcd7
Nguyen Van Viet <nguyenvan.viet@petronas.com>
2022-10-08 11:17:59 +0700

    update UI for couses/show page, add new field for Course schema: video_id

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,10 +1,12 @@
 const Course = require('../models/Course');
+const { mongooseToObject } = require('../../util/mongoose');
 
 class CourseController {
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
             .then((course) => {
-                res.render('courses/show');
+                course = mongooseToObject(course);
+                res.render('courses/show', { course });
             })
             .catch(next);
     }
